ffd7a929c17d17582a0888144e891d4a26c93c25
Aaron Padilla <aapadill@student.hive.fi>
2026-03-29 13:21:39 +0300

    load file, validation, parsing, mapping

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,8 +6,85 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 16:32:00 by aapadill          #+#    #+#             */
-/*   Updated: 2026/03/27 16:32:01 by aapadill         ###   ########.fr       */
+/*   Updated: 2026/03/29 13:02:26 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+void	BitcoinExchange::loadDatabase(const std::string& filename)
+{
+	(void)filename;
+}
+
+void	BitcoinExchange::processInputFile(const std::string& filename) const
+{
+	(void)filename;
+}
+
+std::string	BitcoinExchange::trim(const std::string& str)
+{
+	(void)str;
+	return "";
+}
+
+bool	BitcoinExchange::isValidDateFormat(const std::string& date)
+{
+	(void)date;
+	return false;
+}
+
+bool	BitcoinExchange::isValidDate(const std::string& date)
+{
+	(void)date;
+	return false;
+}
+
+bool	BitcoinExchange::isLeapYear(int year)
+{
+	(void)year;
+	return false;
+}
+
+bool	BitcoinExchange::parseValue(const std::string& str, double& value)
+{
+	(void)str;
+	(void)value;
+	return false;
+}
+
+bool	BitcoinExchange::isPositiveValue(double value)
+{
+	(void)value;
+	return false;
+}
+
+bool	BitcoinExchange::isTooLargeValue(double value)
+{
+	(void)value;
+	return false;
+}
+
+void	BitcoinExchange::parseDatabaseLine(const std::string& line)
+{
+	(void)line;
+}
+
+void	BitcoinExchange::processInputLine(const std::string& line) const
+{
+	(void)line;
+}
+
+double	BitcoinExchange::getRateForDate(const std::string& date) const
+{
+	(void)date;
+	return 0.0;
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,8 +6,40 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 16:32:12 by aapadill          #+#    #+#             */
-/*   Updated: 2026/03/27 16:32:12 by aapadill         ###   ########.fr       */
+/*   Updated: 2026/03/27 18:00:58 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <map>
+#include <string>
+
+class BitcoinExchange
+{
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+
+	void	loadDatabase(const std::string& filename);
+	void	processInputFile(const std::string& filename) const;
+
+private:
+	std::map<std::string, double>	_rates;
+
+private:
+	static std::string	trim(const std::string& str);
+
+	static bool	isValidDateFormat(const std::string& date);
+	static bool	isValidDate(const std::string& date);
+	static bool	isLeapYear(int year);
+
+	static bool	parseValue(const std::string& str, double& value);
+	static bool	isPositiveValue(double value);
+	static bool	isTooLargeValue(double value);
+
+	void	parseDatabaseLine(const std::string& line);
+	void	processInputLine(const std::string& line) const;
+
+	double	getRateForDate(const std::string& date) const;
+};
